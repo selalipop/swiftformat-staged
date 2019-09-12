3cb264ec3d84c96c2895dbdf516c932dc7aa0f79
Selali Adobor <selali.adobor@willowtreeapps.com>
2019-09-12 14:09:45 -0400

    Default to using current directory when running

--- bin/index.js
+++ bin/index.js
@@ -2,10 +2,6 @@
 
 const format = require("../lib/format");
 var args = process.argv.slice(2);
-const repoDir = args[0];
+const repoDir = args[0] || ".";
 
-if (!repoDir) {
-  console.log("Usage: swift-format-staged project/src");
-} else {
-  format.formatStagedChanges(repoDir);
-}
+format.formatStagedChanges(repoDir);
